b7780dd62f262957afe3114fbea7c8a2bb74dd66
SophKovalenko <sophiakovalenko@hotmail.com>
2022-12-05 15:58:33 -0500

    implemented moving platforms

---
+++ Assets/Scripts/PlatformDirection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PlatformDirection
+{
+    HORIZONTAL,
+    VERTICAL,
+    DIAGONAL_UP,
+    DIAGONAL_DOWN,
+    CUSTOM
+}
+
